e144c77c2cdff701df0af5aeee5c2cc30d67a46a
Benourson#9496 <78214390+BenoursonJ@users.noreply.github.com>
2021-03-12 10:39:50 +0100

    Update esx_bankerjob.sql

--- esx_bankerjob.sql
+++ esx_bankerjob.sql
@@ -1,17 +1,17 @@
 INSERT INTO `addon_account` (name, label, shared) VALUES
-	('society_banker', 'Banco', 1)
+	('society_banker', 'Bank', 1)
 ;
 
 INSERT INTO `addon_inventory` (name, label, shared) VALUES
-	('society_banker', 'Banco', 1)
+	('society_banker', 'Bank', 1)
 ;
 
 INSERT INTO `datastore` (name, label, shared) VALUES
-	('society_banker', 'Banco', 1)
+	('society_banker', 'Bank', 1)
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
-	('banker', 'Banqueiro')
+	('banker', 'Banker')
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
